22e0ab8c32653ca022fd8abde2fd05157ccf36df
Ilker Binzet <ilker.binzet@gmail.com>
2024-05-10 10:08:32 -0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -12,6 +12,7 @@ switch($kullaniciSecimi)
 		$jsonYolu = "$env:LOCALAPPDATA\JDownloader 2.0\cfg\org.jdownloader.settings.GraphicalUserInterfaceSettings.json"
 		try {
 			$jsonVerisi = Get-Content -Path $jsonYolu | ConvertFrom-Json
+			Write-Progress -Activity "Dosya Okunuyor" -Status "Tamamlandı" -PercentComplete 33
 		}
 		catch {
 			Write-Error "org.jdownloader.settings.GraphicalUserInterfaceSettings.json dosyası bulunamadı"
@@ -20,13 +21,16 @@ switch($kullaniciSecimi)
 		}
 		$jsonVerisi.statusbaraddpremiumbuttonvisible = $false
 		$jsonVerisi.premiumalertspeedcolumnenabled = $false
+		Write-Progress -Activity "Ayarlar Güncelleniyor" -Status "Tamamlandı" -PercentComplete 66
 
 		$jsonString = $jsonVerisi | ConvertTo-Json
 		$jsonString | Set-Content -Path $jsonYolu
+		Write-Progress -Activity "Dosya Yazılıyor" -Status "Tamamlandı" -PercentComplete 100
 		Write-Output "Ayarlar başarıyla güncellendi."
 		Start-Sleep 3
 	}
 	h {exit}
 	default {Write-Output ("Geçersiz argüman. Cevap e veya h olmalı.")}
 }
+
 
